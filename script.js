f3db8bc7b78468a2ca1bbef9a49232a669a06faa
Danish Zahid <danishst.johns@gmail.com>
2022-10-05 16:53:33 +0530

    done

---
+++ script.js
@@ -0,0 +1,27 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/yoda.json"
+
+function getTranslationURL(param){
+    return serverURL+"?"+"text="+param
+}
+
+function errorHandler(error){
+    alert("something went wrong! Try again after sometime")
+}
+
+function clickEventHandler(){
+    var inputText = txtInput.value;//taking input for processing
+
+    fetch(getTranslationURL(inputText))
+    .then(Response => Response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click",  clickEventHandler)
